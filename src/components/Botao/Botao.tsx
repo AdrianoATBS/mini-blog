d0c8ff1ad1617c33e98e5a4913726732e577b5f4
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-04-13 11:19:06 -0300

    refactor(login): adiciona suporte a className, semHover e semRing no botão

--- src/components/Botao/Botao.tsx
+++ src/components/Botao/Botao.tsx
@@ -1,25 +1,29 @@
+import { ReactNode } from "react";
+
 type Type = "button" | "submit";
    
 interface BotaoProps {
-    texto: string;
+    texto: ReactNode; 
     onClick?: () => void;
     disabled?: boolean;
     type?: Type;
+    className?: string;
+    semHover?: boolean;
+    semRing?: boolean;
 };
 
-export default function Botao({ texto, onClick, disabled, type = "button" }: BotaoProps) {
+export default function Botao({ texto, onClick, disabled, type = "button", className, semHover, semRing }: BotaoProps) {
 
     return(
         
         <>
-            <button type={type} onClick={onClick} disabled={disabled} className="h-10 px-5 rounded-full
+            <button type={type} onClick={onClick} disabled={disabled} className={`h-10 px-5 rounded-full
             bg-acoes-primaria text-white text-suave shadow-sm 
-            transition duration-200 hover:bg-hover hover:shadow-md
+            transition duration-200
+            ${!semHover && 'hover:bg-hover hover:shadow-md'}
             active:scale-95 focus:outline-none focus:ring-2 
-            focus:ring-acoes-primaria disabled:opacity-50
-            disabled:cursor-not-allowed
-           
-            ">
+            ${!semRing && 'focus:ring-acoes-primaria'} disabled:opacity-50
+            disabled:cursor-not-allowed ${className || ''}`}>
                 {texto}
             </button>
            
