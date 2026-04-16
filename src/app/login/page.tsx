1f53cd73f3830f2b928eec4ef721a4a96f93cc9e
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-04-16 10:27:42 -0300

    feat(auth): adiciona submitText e customiza footer com link de cadastro

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,12 +3,12 @@ import Formulario from "@/components/Formulario/Formulario";
 import Checkbox from "@/components/Checkbox/Checkbox";
 import { MdOutlineEmail } from "react-icons/md";
 import { TbLockPassword } from "react-icons/tb";
-
+import Link from "next/link";
 export default function Login(){
    
  
     return(
-        <div className="max-w-7xl mx-auto px-4 pt-10 pb-10">
+        <div className="max-w-7xl mx-auto w-full px-4 pt-10 pb-10">
             <Formulario 
             titulo="Bem vindo de volta!"
             texto="Continue sua jornada"
@@ -17,8 +17,11 @@ export default function Login(){
                 { name: "password", label: "Senha", type: "password", placeholder: "••••••••", icon: <TbLockPassword /> },
             ]}  
             extra={<Checkbox label="Matenha-me conectado" />}
-            footer="Ainda não tem uma conta?"
-            footerLink="Cadastrar-se"
+            footer={<>
+            Ainda não tem uma conta? <Link href="/register" className='text-acoes-primaria font-medium'>Cadastrar-se</Link>
+            </>}
+           
+            submitText="Entrar ->"
             />
         </div>
     )
