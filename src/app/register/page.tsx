96bbd4b488916acdb4dd51a9415136c48d7d66f0
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-04-16 10:30:50 -0300

    feat(auth): configura formulário de registro com textos, placeholders, checkbox e footer com link de login e submiText

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,28 +1,31 @@
 'use client';
 import Botao from "@/components/Botao/Botao";
+import Checkbox from "@/components/Checkbox/Checkbox";
 import Formulario from "@/components/Formulario/Formulario";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 export default function Register(){
 
-    const router = useRouter();
-    
-         const handleVoltar = () => {
-            router.push("/");
-        }
-    
+  
     return(
-        <>
-            <div className="pl-5 pt-6 ">
-                <Botao texto="Voltar" onClick={handleVoltar} />
-            </div>
+        <div className="max-w-7xl mx-auto w-full pb-10 pt-10 ">
             <Formulario 
-            titulo="Registro"   
+            titulo="Crie sua conta"
+            texto="Junte-se à nossa comunidade de curadores editorias."    
             campos={[
-                { name: "nome", label: "Nome", type: "text", placeholder: "Digite seu nome" },
-                { name: "email", label: "Email", type: "email", placeholder: "Digite seu email" },
-                { name: "password", label: "Senha", type: "password", placeholder: "Digite sua senha" },
-                {name: "confirmarPassword", label: "Confirmar Senha", type: "password", placeholder: "Confirme sua senha" },
-            ]}  />
-        </>
+                { name: "nome", label: "Nome", type: "text", placeholder: "Evelyn Harper" },
+                { name: "email", label: "Email", type: "email", placeholder: "name@example.com" },
+                { name: "password", label: "Senha", type: "password", placeholder: "••••••••" },
+                {name: "confirmarPassword", label: "Confirmar Senha", type: "password", placeholder: "••••••••" },
+            ]}  
+            extra={<Checkbox label="Concordo com os Termos de Serviço e Política de Privacidade" />}
+            footer={<>
+                Já tem uma conta? 
+                <Link href="/login" className='text-acoes-primaria font-medium'> Faça login</Link>
+            </>}
+           
+            submitText="Criar uma Conta ->"
+            />
+        </div>
     )
 }
