f18d883c65a07a2e0b76badbd36c3827b113671d
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-04-23 15:21:36 -0300

    refactor(card): reorganizar PostCardUsuario e adiciona cálculo de leitura e estado de bookmark

--- src/components/PostCardUsuario/PostCardUsuario.tsx
+++ src/components/PostCardUsuario/PostCardUsuario.tsx
@@ -1,22 +1,40 @@
+"use client";
 import { Post } from "@/types/Post";
-
+import { MdOutlineBookmarkAdd } from "react-icons/md";
+import { useState } from "react";
 interface PostCardUsuarioProps {
     posts: Post;
     autor: string;
 }
 
 
-export default function PostCardUsuario({posts, autor} : PostCardUsuarioProps) {
-    return(
-        <div>
-            <div className="bg-card border border-borda rounded-xl p-6 shadow-sm 
-            hover:shadow-md transition duration-200 flex flex-col gap-4">
+export default function PostCardUsuario({posts} : PostCardUsuarioProps) {
+    const diasPassados = Math.floor(posts.id % 30) + 1;
+    const dataAleatoria = new Date();
+    dataAleatoria.setDate(dataAleatoria.getDate() - diasPassados);
 
-                <h3 className="titulo-card">{posts.title}</h3>
-                <p className="texto-base">{posts.body}</p>
-                <span className="texto-suave">{autor}</span>
-            </div>
+    const palavras = posts.body.split(/\s+/).length;
+    const minutos = Math.ceil(palavras / 200);
+
+    const [salvo, setSalvo] = useState(false);
     
+    return(
+        <div className="bg-card border border-borda rounded-xl p-6 shadow-sm 
+            hover:shadow-md transition duration-200 flex flex-col gap-4  col-span-1">
+        
+            <h3 className="titulo-card text-start">{posts.title}</h3>
+            <div className=" flex justify-between items-center+ border-t border-borda pt-5 ">
+                <div className="flex gap-4">
+                    <span className="texto-suave ">{dataAleatoria.toLocaleDateString('pt-BR')}</span>
+                    <span className="texto-suave">{minutos} min de leitura</span>
+                </div>
+                <MdOutlineBookmarkAdd
+                onClick={() => setSalvo(!salvo)}
+                className={`self-end cursor-pointer ${salvo ? 'text-acoes-primaria' : 'text-suave'}`}
+                size={24} />
+
+                    
+            </div>
         </div>
     )
 }
