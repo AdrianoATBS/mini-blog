e20372209268d28c16a9753dcd7add5fc86bed67
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-04-27 10:59:03 -0300

    feat(post-card): implementa layout de destaque para PostCrd principal

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,29 +1,29 @@
 import Link from "next/link";
+import { Post } from "@/types/Post";
+import Image from "next/image";
 interface PostCardProps {
-    titulo: string;
-    conteudo: string;
+    post: Post;
     autor: string;
     userId: number;
     postId: number;
 }
-export default function PostCard({titulo, conteudo, autor, userId, postId}: PostCardProps) {
+export default function PostCard({ post, autor, userId, postId }: PostCardProps) {
+
+     const imageUrl = `https://picsum.photos/600/300?random=${post.id}`;
     return(
-        <>
-            <section className=" bg-card p-6 rounded-xl border border-borda 
-            shadow-sm hover:shadow-md transition-shadow duration-300">
-                    <Link className="font-bold titulo-card
-                    text-xl hover:text-acoes-primaria transition duration-300" 
-                    href={`/posts/${postId}`}>{titulo}</Link>
-                    
-                    <p className="text-texto-secundario  line-clamp-3 mt-3
-                    texto-base">{conteudo}</p>
-                   
-                    <Link 
-                        href={`/users/${userId}`} 
-                        className="text-xs text-texto-secundario mt-4 block titulo-card
-                         hover:text-acoes-primaria transition duration-400">{autor}
-                    </Link>
-                </section>
-        </>
+        <section className="flex flex-col bg-card rounded-xl border border-borda col-span-2 row-span-2 
+        shadow-lg ">
+            <Link href={`/posts/${postId}`} className="w-full h-full ">
+                <Image src={imageUrl} alt="Imagem do post" width={600} height={300} 
+                className="w-full h-80 object-cover rounded-t-lg"/>
+                <h2 className="titulo-card pl-4 pt-4 hover:text-acoes-primaria">{post.title}</h2>
+                <p className="texto-base text-texto-secundario p-4">{post.body}</p>
+            </Link>
+
+            <Link href={`/users/${userId}`} className="text-xs text-texto-secundario mt-4 block titulo-card
+                    hover:text-acoes-primaria transition duration-400 cursor-pointer p-4">
+                {autor}
+            </Link>
+    </section>
     )
 }
