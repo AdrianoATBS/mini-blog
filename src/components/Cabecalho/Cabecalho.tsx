c854fd4054ab54598f14a9207a3ccf01df4a7e12
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-05-06 09:48:54 -0300

    feat(cabecalho): adiciona navegação para início, post aleatório e ajusta  estilos

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -14,25 +14,45 @@ export default function Cabecalho() {
 
                     <div className="flex items-center gap-8">
                         <h1 className="titulo-secao tracking-tight">Mini <span className="text-acoes-primaria">Blog</span></h1>
+                        
+                        <h2 className="texto-base text-texto-secundario cursor-pointer hover:text-texto-primario transition duration-200"
+                            onClick={() => userRouter.push("/")}>Início</h2>
+                        <h2 className="texto-base text-texto-secundario cursor-pointer hover:text-texto-primario transition duration-200"
+                            onClick={() => {
+                                const idAleatorio = Math.floor(Math.random() * 100) + 1; 
+                                userRouter.push(`/posts/${idAleatorio}`); 
+                            }}>Posts</h2>
                     </div>
 
-                    <div className="relative w-72">
 
-                        <input
-                            type="text"
-                            placeholder="Pesquisa..."
-                            className="w-full h-10 pl-4 pr-10 border border-borda rounded-lg bg-fundo-secundario
-                            texto-base focus:outline-none focus:ring-1 focus:ring-acoes-primaria 
-                            "
-                        />
-                    
-                        <CiSearch
-                            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-texto-secundario cursor-pointer"
-                        />
-                    </div>  
-                    <div className="flex gap-3 ">
-                        <Botao texto="Registrar" onClick={() => userRouter.push("/register")} />
-                        <Botao texto="Login" onClick={() => userRouter.push("/login")} />
+                    <div className="flex items-center gap-6">
+
+                        <div className="relative w-48">
+
+                            <input
+                                type="text"
+                                placeholder="Pesquisa..."
+                                className="w-full h-10 pl-4 pr-10 border border-borda rounded-2xl bg-fundo-secundario
+                                texto-base focus:outline-none focus:ring-1 focus:ring-acoes-primaria
+                                "
+                                />
+                        
+                            <CiSearch
+                                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-texto-secundario cursor-pointer"
+                                />
+                        </div>  
+                        <div className="flex gap-3 ">
+                            <Botao texto="Registrar" onClick={() => userRouter.push("/register")} 
+                                className="text-acoes-primaria border-none cursor-pointer 
+                                "
+                                semBackground={true}
+                                semHover={true}
+                                semRing={true}
+                                semWhite={true}
+                                />
+                            <Botao texto="Login" onClick={() => userRouter.push("/login")} 
+                                className="cursor-pointer"/>
+                        </div>
                     </div>
                    
                 </div>
