6de4f05f3b4916c4fd089d10dfcd0c1f5416626b
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-05-06 09:54:00 -0300

    refactor(home0: remove cabeçalho local

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Cabecalho from "@/components/Cabecalho/Cabecalho";
+
 import HomeClient from "@/components/HomeClient/HomeClient";
 import { getPosts } from "@/service/posts.service";
 import { getUsers } from "@/service/users.service";
@@ -10,8 +10,6 @@ export default async function Home() {
   return (
      < >
       <div className="bg-fundo min-h-screen flex flex-col">
-
-      <Cabecalho />
 
       <HomeClient posts={posts} users={users} />
 
