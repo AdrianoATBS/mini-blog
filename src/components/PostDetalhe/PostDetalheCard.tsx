5d60571a4524d62b2cc4c9fa7ea99ce10fec63f5
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-05-11 10:33:36 -0300

    style(post-detalhe-card): ajusta layout para mobile

--- src/components/PostDetalhe/PostDetalheCard.tsx
+++ src/components/PostDetalhe/PostDetalheCard.tsx
@@ -30,37 +30,37 @@ export default function PostDetalheCard({post, user, comentario}: {post: Post, u
         setStatus(prev => ({ ...prev, [campo]: !prev[campo] }));
     }
     return(
-        <div className="m">
-            <div className=" p-10 flex flex-col justify-center  gap-6">
-                <h1 className="titulo-principal">
+        <div className="">
+            <div className="p-4 md:p-6 lg:p-10 flex flex-col justify-center gap-6">
+                <h1 className="text-lg font-bold lg:titulo-principal">
                     {post?.title}
                 </h1>
                 <div className="flex bg-[#F2F4F6] p-4 rounded-lg items-center">
-                    <FaUserCircle className="inline-block text-2xl text-gray-500 mr-2" />
-                    <span className="titulo-card">
+                    <FaUserCircle className="hidden lg:inline-block text-2xl text-gray-500 mr-2" />
+                    <span className="text-[0.675rem] lg:titulo-card">
                         {user ? user.name : "Autor Desconecido"}
                     </span>
-                    <div className="flex justify-center items-center gap-4 ml-auto ">
-                        <IoShareSocialOutline size={20} onClick={() => toggleStatus('compartilhado')} 
-                        className={`cursor-pointer ${status.compartilhado ? 'text-acoes-primaria' : 'text-gray-500'}`} />
-                        <FaRegBookmark size={20} onClick={() => toggleStatus('salvo')} 
-                        className={`cursor-pointer ${status.salvo ? 'text-acoes-primaria' : 'text-gray-500'}`} />
+                    <div className="flex justify-center items-center gap-3 lg:gap-5 ml-auto  ">
+                        <IoShareSocialOutline onClick={() => toggleStatus('compartilhado')} 
+                        className={`cursor-pointer text-sm lg:text-xl ${status.compartilhado ? 'text-acoes-primaria' : 'text-gray-500'}`} />
+                        <FaRegBookmark  onClick={() => toggleStatus('salvo')} 
+                        className={`cursor-pointer text-sm lg:text-xl ${status.salvo ? 'text-acoes-primaria' : 'text-gray-500'}`} />
                     </div>
                 </div>
                 <Image src={imageUrl} alt="Imagem do post" width={1000} height={300} className="rounded-lg" />
-                <p className="texto-base mt-4">{post?.body}</p>
+                <p className="text-[0.775rem] lg:texto-base mt-4">{post?.body}</p>
                 
-                <div className="flex justify-start items-center gap-4 mt-4 border 
+                <div className="flex justify-start items-center gap-2 lg:gap-4 mt-4 border 
                 border-borda/25 rounded-lg px-4 py-2">
                     <div className="flex justify-center items-center gap-2">
-                        <FaHeart size={20} 
+                        <FaHeart 
                         onClick={() => toggleStatus('curtido')}
-                        className={`cursor-pointer ${status.curtido ? 'text-acoes-primaria' : 'text-gray-500'}`}     />
-                        <span>{likes}</span>
+                        className={`cursor-pointer text-sm lg:text-xl ${status.curtido ? 'text-acoes-primaria' : 'text-gray-500'}`}     />
+                        <span className="text-sm lg:text-xl">{likes}</span>
                     </div>
                     <div className="flex justify-center items-center gap-2">
-                        <RiMessage3Line size={20} className=" text-gray-500 ml-4" />
-                        <span>{comentario?.length || 0}</span>
+                        <RiMessage3Line className=" text-gray-500 ml-4 text-xs lg:text-xl" />
+                        <span className="text-sm lg:text-xl">{comentario?.length || 0}</span>
                     </div>
                 </div>
             </div>
