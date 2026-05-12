44e5bc7ed1e6e8ad0e79cb3d96b7d86439f5597f
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-05-12 09:41:50 -0300

    style(informacao-users): ajusta layout responsivo para mobile

--- src/components/InformacaoUsers/InformacaoUsers.tsx
+++ src/components/InformacaoUsers/InformacaoUsers.tsx
@@ -11,36 +11,41 @@ import { Post } from "@/types/Post";
 export default function InformacaoUsers({ user, posts }: { user: UserPerfil; posts: Post[] }) {
     return(
         <section className="bg-card border border-borda rounded-2xl p-8 shadow-lg flex flex-col gap-8">
-            <div className="flex justify-between items-start gap-6">
+            <div className="flex flex-col items-center gap-6 sm:flex-col sm:items-center md:flex-row 
+            md:justify-between lg:flex-row lg:justify-between ">
 
-                <div className=" flex justify-center items-center w-30 h-30 bg-gray-300/45 rounded-xl relative">  
-                    <Image src="/UserAvatar.png" alt="Avatar do usuário" width={120} height={120} className="rounded-xl" />
+                <div className=" flex justify-center items-center w-36  lg:w-[120px] lg:h-[120px] 
+                 bg-gray-300/45  rounded-xl relative">  
+                    <Image src="/UserAvatar.png" alt="Avatar do usuário" width={120}
+                     height={120} className="rounded-xl" />
                     
                     <div className=" flex justify-center items-center
                      bg-acoes-primaria w-7 h-7 rounded-xl absolute -bottom-3 -right-3 ">
                         <FaCheckCircle className="text-white" size={22} />   
                         
-                    </div>
+                </div>
                     
                 </div>
                 <div className="flex-1 flex flex-col gap-4">
-                    <h3 className="texto-suave text-acoes-primaria bg-fundo-secundario w-fit rounded-lg px-2 ">Colaborador Editorial</h3>
-                    <h2 className="titulo-principal">{user.name}</h2>
+                    <h3 className="text-[0.768rem] lg:texto-suave text-acoes-primaria
+                     bg-fundo-secundario w-fit rounded-lg px-2 ">Colaborador Editorial</h3>
+                    <h1 className="text-lg font-bold text-titulo lg:titulo-principal">{user.name}</h1>
 
                     <div className="flex flex-wrap items-center gap-4 ">
-                        <FaRegBuilding className="text-acoes-primaria" size={20} />
-                        <p className="texto-suave">{user.company.name}</p>
+                        <FaRegBuilding className="text-acoes-primaria text-sm md:text-base lg:text-lg" />
+                        <p className="text-sm text-texto-secundario md:text-base lg:texto-suave">{user.company.name}</p>
 
-                        <MdOutlineEmail className="text-acoes-primaria" size={20} />
-                        <span className="texto-suave">{user.email}</span>
+                        <MdOutlineEmail className="text-acoes-primaria text-sm md:text-base lg:text-lg" />
+                        <span className=" text-sm text-texto-secundario  md:text-base lg:texto-suave">{user.email}</span>
 
-                        <FaPhoneAlt className="text-acoes-primaria" size={20} />
-                        <span className="texto-suave">{user.phone}</span>
+                        <FaPhoneAlt className="text-acoes-primaria text-sm md:text-base lg:text-lg"  />
+                        <span className="text-[0.675rem] text-texto-secundario md:text-base lg:texto-suave">{user.phone}</span>
 
-                        <TbWorld className="text-acoes-primaria" size={20} />
+                        <TbWorld className="text-acoes-primaria text-sm md:text-base lg:text-lg"  />
                             <Link href={`https://${user.website}`} target="_blank" rel="noopener noreferrer"
-                            className="texto-base text-acoes-primaria hover:underline">{user.website}</Link>
-                        <p className="texto-suave">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ipsum dolor fugiat non sint doloribus blanditiis ut, perferendis recusandae a
+                            className="text-sm  md:text-base lg:texto-suave text-acoes-primaria hover:underline">{user.website}</Link>
+                        
+                        <p className="text-sm text-texto-secundario md:text-base lg:texto-suave">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ipsum dolor fugiat non sint doloribus blanditiis ut, perferendis recusandae a
                             d officia nemo aliquam voluptates culpa reprehenderit delectus vel. At, voluptatibus.</p>
 
                     </div>
@@ -48,11 +53,11 @@ export default function InformacaoUsers({ user, posts }: { user: UserPerfil; pos
 
                 <div className="flex flex-col gap-4">
                     <div className="bg-gray-100 rounded-xl px-4 py-2 text-center">
-                        <p className="font-bold">{posts.length}</p>
+                        <p className=" text-sm lg:text-base font-bold">{posts.length}</p>
                         <span className="texto-suave">Posts</span>
                     </div>
                     <div className="bg-gray-100 rounded-xl px-4 py-2 text-center"   >
-                        <p className="font-bold">8.2k</p>
+                        <p className="text-sm lg:text-lg font-bold">8.2k</p>
                         <span className="texto-suave">Seguidores</span> 
                     </div>
                 </div>
--- src/components/PerfilCabecalho/PerfilCabecalho.tsx
+++ src/components/PerfilCabecalho/PerfilCabecalho.tsx
@@ -1,14 +1,13 @@
 'use client';
 import InformacaoUsers from "../InformacaoUsers/InformacaoUsers";
 import { UserPerfil } from "../../types/UserPerfil";
-import Botao from "../Botao/Botao";
-import {useRouter} from "next/navigation";
+
 import { Post } from "@/types/Post";
 export default function PerfilCabecalho({ user, posts }: { user: UserPerfil; posts: Post[] }) {
    
 
     return(
-        <div className="max-w-6xl  mx-auto px-6 py-10 flex flex-col gap-6">
+        <div className="max-w-6xl mx-auto px-6 py-10 flex flex-col gap-6">
 
             <InformacaoUsers user={user} posts={posts} />
 
